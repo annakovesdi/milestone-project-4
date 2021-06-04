db1f9527c4c5196f846f4c21169a5d0182a29112
annakovesdi <annakovesdi@gmail.com>
2021-06-04 08:20:47 +0000

    double navbar top customized

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 process:
 
 -secret key visible in github
+-followed course way of setting up shop
+-https://docs.djangoproject.com/en/2.0/ref/templates/builtins/#spaceless
---
+++ static/js/materialize.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.modal').modal();
+    $('.dropdown-trigger').dropdown(open(hover=true));
+    $('.collapsible').collapsible();
+    $('.tabs').tabs();
+  });
